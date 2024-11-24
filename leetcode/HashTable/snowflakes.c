42c6cbb7be81a597b37bdaf7d33a79c8fda4d983
Otmane <otmanelbo123@gmail.com>
2024-11-24 09:11:33 +0100

    Modified

--- leetcode/HashTable/snowflakes.c
+++ leetcode/HashTable/snowflakes.c
@@ -27,7 +27,7 @@ int identical_right(int snow1[], int snow2[], int start)
     int offset = 0;
     while (offset < 6)
     {
-        if(snow1[offset] != snow2[(start + offset) % 6])
+        if (snow1[offset] != snow2[(start + offset) % 6])
         {
             return 0;
         }
@@ -42,11 +42,11 @@ int identical_left(int snow1[], int snow2[], int start)
     snow2_index = offset - start;
     while (offset < 6)
     {
-        if( snow2_index < 0)
+        if (snow2_index < 0)
         {
             snow2_index = snow2_index + 6;
         }
-        if(snow1[offset] != snow2[snow2_index])//we can not use the reminder here 
+        if (snow1[offset] != snow2[snow2_index]) // we can not use the reminder here
         {
             return 0;
         }
@@ -54,49 +54,57 @@ int identical_left(int snow1[], int snow2[], int start)
     }
     return 1;
 }
-int are_identecal(int snow1[], int snow2[])
+int are_identical(int snow1[], int snow2[])
 {
     int start = 0;
     while (start < 6)
     {
-        if(identical_right(snow1, snow2, start))
+        if (identical_right(snow1, snow2, start))
         {
             return 1;
         }
-         if(identical_left(snow1, snow2, start))
+        if (identical_left(snow1, snow2, start))
         {
             return 1;
         }
         start++;
     }
     return 0;
-
 }
-int ft_check(int arr[][6])
+void identify_identical(int snowflakes[][6], int n)
 {
-    int i = 0;
-    int l = 0;
-    int k = 1;
-    int j = 0;
-    while (i < 6)
+    int i, j;
+    for (i = 0; i < n; i++)
     {
-        while (j < 6)
+        for (j = i + 1; j < n; j++)
         {
-            if (arr[l][i] == arr[k][j])
+            if (are_identical(snowflakes[i], snowflakes[j]))
             {
-                j++;
-                break
+                printf("Twin snowflakes found.\n");
+                return;
             }
         }
     }
+    printf("No two snowflakes are alike.\n");
 }
+#define SIZE 100000
 int main()
 {
     /*In the case of a two-dimensional array, you are passing a pointer to an array of arrays.
      This means you need to explicitly define the size of at least the second dimension when
       passing it to a function. The compiler needs to know how to
       calculate the memory offsets when accessing array elements.*/
-    int snowflakes[2][6] = {{1, 2, 3, 4, 5, 6}, {1, 2, 3, 4, 5, 6}};
 
-    ft_check(snowflakes);
+        static int snowflakes[SIZE][6];
+        int n, i, j;
+        scanf("%d", &n);
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < 6; j++)
+                scanf("%d", &snowflakes[i][j]);
+        identify_identical(snowflakes, n);
+
+        }
+        return 0;
+    
 }
